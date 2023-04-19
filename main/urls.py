19f663ae9e5bf3a142e26f2ed5e26d8ac3a5042d
Ancient Legend <gtermix7@gmail.com>
2023-04-19 15:48:12 +0500

    Update

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', main_page),
-    path('about/', about_page),
-    path('contact/', contact_page)
+    path('about/', about_page, name="about"),
+    path('contact/', contact_page, name="contact"),
+    path('registration/', registration_page, name="registration")
 ]
--- main/views.py
+++ main/views.py
@@ -3,16 +3,16 @@ from django.http import HttpRequest, HttpResponse
 
 
 def main_page(request: HttpRequest):
-    return HttpResponse(
-        """<h1 style="color:grey;text-align:center;">Hello World</h1></br><a style="text-decoration: none;" href="/about/">About Us
-        \t</a><a style="text-decoration: none;" href="/contact/">Contact with admin</a>""")
+    return render(request, "main/index.html")
 
 
 def about_page(request: HttpRequest):
-    return HttpResponse("""<h1 style="color:grey;text-align:center;">About Us</h1></br><a style="text-decoration: none;" href="/">Main page 
-    \t</a><a style="text-decoration: none;" href="/contact/">Contact with admin</a><p>lorem500</p>""")
+    return render(request, "main/about.html")
 
 
 def contact_page(request: HttpRequest):
-    return HttpResponse("""<h1 style="color:grey;text-align:center;">Contact Us</h1></br><a style="text-decoration: none;" href="/">Main page 
-    \t</a><a style="text-decoration: none;" href="/about/">About Us</a></br><a href="https://t.me/yuldashevb_0221">Contact with telegram</a>""")
+    return render(request, "main/contact.html")
+
+
+def registration_page(request: HttpRequest):
+    return render(request, "main/registration.html")
