46f3773e0f9b2fb10fb534e0bd2caeb2e1ec1ca4
Ancient Legend <gtermix7@gmail.com>
2023-05-22 15:42:39 +0500

    fix: added class views

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', main)
+    path('', main),
+    path('cats/detail/', CatDetailCreateView.as_view(), name='cr_cat'),
+    path('cat/<int:pk>/', CatDeleteEditView.as_view(), name='sdfsd'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,64 @@
-from django.shortcuts import render
+import json
+import logging
+
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from rest_framework import status
+from main.models import Product, Category
+from django.forms import model_to_dict
+from .serializers import CategorySerializer
+from rest_framework.generics import GenericAPIView
 
 
-@api_view(http_method_names=['GET', 'POST'])
+@api_view(http_method_names=['GET'])
 def main(req: Request):
-    if req.method == 'POST':
-        return Response(data={'hello': 'world'}, status=HTTP_201_CREATED)
-    return JsonResponse(data={'salom': 'dunyo'})
+    products = Product.objects.all().values()
+    return Response(data=list(products))
+
+
+class CatDetailCreateView(GenericAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def post(self, req):
+        serializer = self.get_serializer_class()(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get(self, req):
+        serializer = self.get_serializer_class()(Category.objects.all(), many=True)
+        return Response(data=serializer.data)
+
+
+class CatDeleteEditView(GenericAPIView):
+    lookup_field = 'pk'
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def get(self, req, pk):
+        cat = get_object_or_404(Category, pk=pk)
+        serializer = self.get_serializer_class()(cat)
+        return Response(data=serializer.data)
+
+    def patch(self, req, pk):
+        cat = get_object_or_404(Category, pk=pk)
+        serializer = self.get_serializer_class()(cat, data=req.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(cat, serializer.validated_data)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, req, pk):
+        cat = get_object_or_404(Category, pk=pk)
+        serializer = self.get_serializer_class()(cat, data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(cat, serializer.validated_data)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, req, pk):
+        cat = get_object_or_404(Category, pk=pk)
+        cat.delete()
+        return Response({}, status.HTTP_204_NO_CONTENT)
